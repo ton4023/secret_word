dad77a16101d3cc455f4068cfe7ca21123ac8921
ton4023 <ton4023@gmail.com>
2021-04-24 11:16:36 +0700

    add random word

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ import random
 
 score = 0
 lives = 3
-words = ['lisa', 'jennine', 'jisooo', 'rose']
+words = ['lisa', 'jennine', 'jisoo', 'rose']
 
 
 def update_clue(guess, secret_word, clue):
     for i in range(len(secret_word)):
         if guess == secret_word[i]:
             clue[i] = guess
+    result = ''.join(clue) == secret_word
+    return result
 
-    win = ''.join(clue) == secret_word
-    return win
+
+def hint_clue(clue, secret_word):
+    rnd_word = random.randint(0, len(secret_word)-1)
+    list_word = list(secret_word)
+    for i in range(len(secret_word)):
+        if list_word[i] == secret_word[rnd_word]:
+            clue[i] = secret_word[rnd_word]
+    return clue
+
+
 
 
 while (len(words) > 0) and (lives > 0):
@@ -20,7 +30,7 @@ while (len(words) > 0) and (lives > 0):
     clue = list('?' * len(secret_word))
 
     while True:
-        print(clue)
+        print(hint_clue(clue, secret_word))
         guess = input('ป้อนคำ : ')
         if guess in secret_word:
             win = update_clue(guess, secret_word, clue)
@@ -30,16 +40,12 @@ while (len(words) > 0) and (lives > 0):
                 score = score + 1
                 print('score' + str(score))
                 break
-            else:
-                print('ผิด')
-                lives = lives - 1
-                print('จำนวนชีวิต ' + str(lives))
-                if lives == 0:
-                    print('เฉลย ' + secret_word)
-                    break
+
+        else:
+            print('ผิด' + str(lives))
+            lives = lives - 1
+            if lives == 0:
+                print('เฉลย ' + secret_word)
+                break
 
 
-print('---------------------------')
-print('end game')
-print('score ' + str(score))
-print('---------------------------')
